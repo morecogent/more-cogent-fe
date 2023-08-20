370ce7fe34c1b2f16a55f7011c9dd3fc7a9755b2
Oskar <oskarkaminski.dev@gmail.com>
2023-08-20 15:09:55 +0200

    Added activity types (yet not used)

--- src/domains/Activities/models/Activity.model.ts
+++ src/domains/Activities/models/Activity.model.ts
@@ -2,17 +2,26 @@ import DesignTreeItem from "../../Design/models/DesignTreeItem.model"
 import {computed, makeObservable, observable} from "mobx";
 import {IDesignTreeItemProperties} from "../../Design/models/DesignTreeItem.types";
 
-interface ActivityProperties extends IDesignTreeItemProperties {
+interface IActivity {
     name: string
+    type: 'HABIT' | 'SINGLE'
 }
 
-export default class Activity extends DesignTreeItem {
+interface ActivityProperties extends IDesignTreeItemProperties {
     name: string
+    type?: 'HABIT' | 'SINGLE'
+}
+
+export default class Activity extends DesignTreeItem implements IActivity {
+    name
+    type
 
 
-    constructor({id, name, parentId, justificationIds}: ActivityProperties) {
+    constructor({id, name, parentId, justificationIds, type}: ActivityProperties) {
         super({id, parentId, justificationIds})
         this.name = name
+        this.type = type || 'SINGLE'
+
 
         makeObservable(this, {
             name: observable
